c1beb104cb5cd1f3867263efa90aec29512451c3
ruther <ruther2525@gmail.com>
2022-09-23 18:58:16 +0900

    🍵Modified docusaurus config and GitHub Workflow yaml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  title: '創営会HTML講座',
+  tagline: 'イチから自分の手でポートフォリオサイトを作ってみよう！',
+  url: 'https://soueikai-programming.github.io',
+  baseUrl: '/SoeikaiHTMLLesson/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'soueikai-programming', // Usually your GitHub org/user name.
+  projectName: 'SoeikaiHTMLLesson', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
